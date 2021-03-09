e9e5ab083dff225c373f46ca7fff636c966b1e64
yeonhong park <yeonhong84@gmail.com>
2021-03-09 14:53:04 +0900

    raycast target 일괄 제거, maskalbe 일괄 제거

---
+++ Assets/Scripts/Editor/UncheckRaycastTarget.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public static class UIImageOptimization
+{
+	[MenuItem("GameObject/CustomMenu/Optimization/모든 Raycast Target 끄기", false)]
+	static void RaycastTargetOff() {
+		List<GameObject> rootObjects = new List<GameObject>();
+		Scene scene = SceneManager.GetActiveScene();
+		scene.GetRootGameObjects(rootObjects);
+
+		for (int i = 0; i < rootObjects.Count; ++i) {
+			GameObject gameObject = rootObjects[i];
+			if (gameObject.transform.childCount > 0) {
+				FindChild(gameObject.transform, (t) => {
+					var img = t.GetComponent<Image>();
+					if(img != null) {
+						img.raycastTarget = false;
+					}
+				});
+			}
+		}
+	}
+
+	[MenuItem("GameObject/CustomMenu/Optimization/모든 Maskable 끄기", false)]
+	static void Maskable() {
+		List<GameObject> rootObjects = new List<GameObject>();
+		Scene scene = SceneManager.GetActiveScene();
+		scene.GetRootGameObjects(rootObjects);
+
+		for (int i = 0; i < rootObjects.Count; ++i) {
+			GameObject gameObject = rootObjects[i];
+			if (gameObject.transform.childCount > 0) {
+				FindChild(gameObject.transform, (t) => {
+					var img = t.GetComponent<Image>();
+					if (img != null) {
+						img.maskable = false;
+					}
+				});
+			}
+		}
+	}
+
+	private static void FindChild(Transform tRoot, Action<Transform> action) {
+		for (int f = 0; f < tRoot.childCount; f++) {
+			var child = tRoot.GetChild(f);
+			action(child);
+			if (child.transform.childCount > 0) {
+				FindChild(child, action);
+			}
+		}
+	}
+}
